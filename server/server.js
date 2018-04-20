dca1e8517d817a5efd2f146ffb8d3bf45b437f75
pablorcruh <pablorcruh@gmail.com>
2018-04-20 15:48:01 -0500

    add delete todo path

--- server/server.js
+++ server/server.js
@@ -52,6 +52,23 @@ app.get('/todos/:id',(req,res)=>{
 
 });
 
+app.delete('/todos/:id',(req,res)=>{
+    // get the id
+    var id=req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((doc)=>{
+        if(!doc){
+            return res.status(404).send();
+        }
+        res.status(200).send(doc);
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+    
+});
+
 app.listen(port,()=>{
     console.log('Started on port 3000');
 });
