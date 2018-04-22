2d17c72a9ed1d7e187ad2b77e2e78f666b67fb08
pablorcruh <pablorcruh@gmail.com>
2018-04-22 13:41:53 -0500

    add POST /users/login

--- server/server.js
+++ server/server.js
@@ -101,7 +101,8 @@ app.patch('/todos/:id',(req,res)=>{
 
 
 app.post('/users',(req,res)=>{
-    var user=new Users(_.pick(req.body,['email','password']));
+    var body=_.pick(req.body,['email','password']);
+    var user=new Users(body);
     user.save().then(()=>{
         return user.generateAuthToken();
     }).then((token)=>{
@@ -111,10 +112,23 @@ app.post('/users',(req,res)=>{
     });
 });
 
-
 app.get('/users/me', authenticate,(req,res)=>{
     res.send(req.user);
 });
+
+
+// POST /users/login {email password}
+app.post('/users/login',(req,res)=>{
+    var body=_.pick(req.body,['email','password']);
+
+    Users.findByCredentials(body.email,body.password).then((user)=>{
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+})
 
 app.listen(port,()=>{
     console.log('Started on port 3000');
