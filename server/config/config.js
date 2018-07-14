59f20321bcef8b33bb6416df608c5c748131de4d
pablorcruh <pablorcruh@gmail.com>
2018-07-13 23:36:23 -0500

    remove console.log from code

--- server/config/config.js
+++ server/config/config.js
@@ -3,9 +3,7 @@ var env=process.env.NODE_ENV || 'development';
 if(env==='development' || env ==='test'){
     var config=require('./configuration');
     var envConfig=config[env];
-    console.log('>>>>>>>>>>>>',envConfig);
     Object.keys(envConfig).forEach((key)=>{
         process.env[key]=envConfig[key];
-        console.log('----------',envConfig[key]);
     });
 }
